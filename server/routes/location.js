fdacab79662e4fce174f75f025903861df5e11ec
christuttle <crunch.tuttle@gmail.com>
2016-04-13 17:02:24 -0500

    add lots of stupid names and birthplaces

---
+++ server/routes/location.js
@@ -0,0 +1,5 @@
+var location = ["Mordor", "The Shire", "Rivendell", "Isengard", "Minias Tirith",
+"Bree", "Rohan", "Carn Dum", "Moria", "The Lonley Mountain", "Norgod", "Minas Morgul",
+"Mount Doom", "Weathertop", "Gondor", "Osgiliath", "Fangor", "Helms Deep", "Dunharrow", "St. Paul"];
+
+module.exports = location;
---
+++ server/routes/name.js
@@ -0,0 +1,2 @@
+names = ['Anakin Skywalker', 'Obi-Wan Kenobi', 'R2-D2', 'JarJar Binks', 'Kylo Ren', 'BB-8', 'Rey', 'Lando Calrissian', 'Finn', 'Poe Dameron',
+'Yoda', 'Luke', 'Mace Windo', 'Count Dooku', 'Han Solo', 'Chewbacca', 'Boba Fett', 'Leia Organa', 'Jean-Luc Piccard', 'Bob']
