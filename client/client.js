db8a092ab8340d23efa0f7fae7977c2be4d36427
sasham43 <sashasemail@gmail.com>
2016-04-14 11:28:00 -0500

    Hard mode working

--- client/client.js
+++ client/client.js
@@ -5,18 +5,30 @@ app.controller('BlueController', ['$scope', '$http', function($scope, $http){
   $scope.userList = [];
 
   $scope.getPeople = function() {
-    $http.get('/people').then(function(response){
+    console.log('getpeople ran');
+    $http.get('/create').then(function(response){
       console.log(response.data);
+      $scope.userList = [];
       $scope.userList = response.data;
+
       if (response.status !== 200){
         console.log('Error on getting data')
       }
-      $scope.user = {};
-      $scope.userList = response.data;
-      return response.data;
+      // $scope.user = {};
+      // $scope.userList = response.data;
+      // return response.data;
 
       console.log(response.data);
 
-    })
+    });
+  };
+
+  $scope.deletePerson = function(person, index){
+    console.log("index", index);
+    var id = person._id;
+    $http.delete('/delete/' + id).then(function(response){
+      console.log('ajax delete call made');
+    });
+    $scope.userList.splice(index, 1);
   }
 }])
--- server/routes/index.js
+++ server/routes/index.js
@@ -10,25 +10,38 @@ router.get('/', function(request, response){
   response.sendFile(path.join(__dirname, '../public/views/index.html'));
 });
 
-
-router.get('/people', function(request, response){
-  var data = request.body;
-  userModel.find({}, function(err, people){
-    if(err){
-      console.log(err);
-      response.sendStatus(500);
-    } else {
-        response.send(people);
-    }
-
-  })
-});
+// not working, router.get('/create') functions so no need for this
+// router.get('/people', function(request, response){
+//   var data = request.body;
+//   userModel.find({}, function(err, people){
+//     if(err){
+//       console.log(err);
+//       response.sendStatus(500);
+//     } else {
+//         response.send(people);
+//     }
+//
+//   })
+// });
 
 router.get('/create', function(request, response){
   console.log('server works');
   response.send(users());
   response.sendStatus(200);
-})
+});
+
+router.delete('/delete/:id', function(request, response){
+  console.log('Delete request received');
+  userModel.findOneAndRemove({_id: request.params.id}, function(err, person){
+    if (err){
+      console.log(err);
+      response.sendStatus(500);
+    } else {
+      console.log('Person deleted: ', person);
+      response.sendStatus(200);
+    }
+  });
+});
 
 
 
