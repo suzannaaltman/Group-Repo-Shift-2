923ae54b5efdc973c5b832ca6945f67ae99ba21a
LisaMabley <lisamabley@gmail.com>
2016-04-14 12:50:03 -0500

    invokes method to add users to db. works. needs css

--- client/client.js
+++ client/client.js
@@ -1,26 +1,21 @@
 var app = angular.module('peopleApp', []);
 
-
 app.controller('PeopleController', ['$scope', '$http', function($scope, $http){
   // client side code
   $scope.users = [];
 
   $scope.getPeople = function(){
     $http.get('/all').then(function(response){
-      console.log(response);
       $scope.users = response.data;
       console.log($scope.users);
     })
 
   }
-  $scope.removeUser = function(user){
+  $scope.removeUser = function(user) {
     userId = user._id;
-    console.log(userId);
     $http.delete('/remove/' +userId).then(function(response){
         console.log('deleted', user);
         $scope.getPeople();
-        //TODO: set up endpoint
     })
   }
-
 }]);
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,35 +3,23 @@ var users = require("../modules/createTwentyUsers");
 var User = require('../../models/usermodel');
 var path = require('path');
 
-
 var router = express.Router();
-
-
 
 router.get("/", function(request, response) {
   response.sendFile(path.join(__dirname, '../public/views/index.html'));
-
 });
 
 router.get("/all", function(resquest, response) {
-  console.log('This is working');
+  users();
   User.find({}, function(err, people){
-    console.log('router find users in db');
     if(err){
       console.log(err);
       response.sendStatus(500);
     } else {
-      console.log('index.js router', people);
       response.send(people);
     }
   })
 })
-
-router.get("/create", function(request, response){
-  response.send(users());
-  response.sendStatus(200);
-})
-
 
 router.delete('/remove/:id', function(request, response){
   console.log('deleted object');
@@ -39,7 +27,6 @@ router.delete('/remove/:id', function(request, response){
     if(err) {console.log('delete err', err);}
   });
   response.sendStatus(200);
-
 })
 
 module.exports = router;
